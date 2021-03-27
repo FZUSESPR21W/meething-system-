29a8529fa0eedf1607c645a94a076ff68ec0430d
aaagx <54830165+aaagx@users.noreply.github.com>
2021-03-27 18:58:34 +0800

    ID自增问题

--- forum/src/main/java/team/concerto/forum/entity/Message.java
+++ forum/src/main/java/team/concerto/forum/entity/Message.java
@@ -1,5 +1,7 @@
 package team.concerto.forum.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Message {
-
+  @TableId(type= IdType.AUTO)
   private long mid;
   private long publisher;
   private long fid;
