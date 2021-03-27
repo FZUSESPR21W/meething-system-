f16dae67e8d56bf69138bd5b33d2ff84fbd655eb
MHero-DarkLaw <syanscor@gmail.com>
2021-03-27 17:32:24 +0800

    获取用户所在的论坛

---
+++ forum/src/main/java/team/concerto/forum/service/UserforumService.java
@@ -0,0 +1,8 @@
+package team.concerto.forum.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import team.concerto.forum.entity.Forum;
+import team.concerto.forum.entity.Userforum;
+
+public interface UserforumService extends IService<Userforum> {
+}
---
+++ forum/src/main/java/team/concerto/forum/service/impl/ForumServiceImpl.java
@@ -0,0 +1,11 @@
+package team.concerto.forum.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+import team.concerto.forum.entity.Forum;
+import team.concerto.forum.mapper.ForumMapper;
+
+@Service
+public class ForumServiceImpl extends ServiceImpl<ForumMapper, Forum> {
+
+}
